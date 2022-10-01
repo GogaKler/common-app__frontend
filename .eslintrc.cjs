47e55a184a4ad577b1202ae0c66f944f411c5b15
Egor Kolesnikov <gogakler@gmail.com>
2022-10-01 14:58:01 +0300

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  "root": true,
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier"
+  ],
+  "rules": {
+    'prettier/prettier': [
+      'warn',
+      {
+        singleQuote: true,
+        semi: true,
+        trailingComma: 'none'
+      }
+    ]
+  },
+  "parserOptions": {
+    "ecmaVersion": "latest"
+  }
+}
---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header class="mr-4 ml-4">
+    <div class="container"></div>
+  </header>
+  <RouterView />
+</template>
