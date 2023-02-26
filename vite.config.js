5b108e812b76fadb774612893b7cb42b420decb1
GogaKler <gogakler@gmail.com>
2023-02-26 23:33:31 +0300

    env update

--- vite.config.js
+++ vite.config.js
@@ -62,9 +62,6 @@ export default defineConfig(({ mode }) => {
         },
         json: {
             stringify: true
-        },
-        build: {
-            sourcemap: isDev
         }
     };
 });
